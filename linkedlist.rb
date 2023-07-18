09cf7046bfa313345388a6b5304042557196a585
Leonardo Fragozo <leo.fragozo.dev@gmail.com>
2023-07-18 11:52:01 -0300

    Add the head, tail and at(index) methods from the exercise

--- linkedlist.rb
+++ linkedlist.rb
@@ -59,12 +59,50 @@ class LinkedList
       current = current.next_node
     end
   end
-end
 
-list = LinkedList.new
-list.append(1)
-list.append(2)
-list.prepend(0)
-list.display
-puts "Size: #{list.size}"
-puts "Head: #{list.head.value}"
+  def head
+    @head
+  end
+
+  def tail
+    return nil if @head.nil?
+
+    current = @head
+    current = current.next_node until current.next_node.nil?
+    current
+  end
+
+  def at(index)
+    return nil if @head.nil? || index < 0
+
+    current = @head
+    i = 0
+
+    while i < index && !current.nil?
+      current = current.next_node
+      i += 1
+    end
+
+    current
+  end
+
+  list = LinkedList.new
+  list.append(1)
+  list.append(2)
+  list.prepend(0)
+  list.display
+
+  puts "Size: #{list.size}"
+  puts "Head: #{list.head.value}"
+  puts "Tail: #{list.tail.value}"
+
+  puts "Enter the index to retrieve the node value:"
+  index = gets.chomp.to_i
+
+  node = list.at(index)
+  if node.nil?
+    puts "Node at index #{index} does not exist."
+  else
+    puts "Node at index #{index}: #{node.value}"
+  end
+end
